af8e9368367f9866a16f5825f00d7c8c63ddb8b3
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2024-12-18 12:52:50 -0800

    removing asahi too complex right now

--- hosts/MBP-M1-VM/default.nix
+++ hosts/MBP-M1-VM/default.nix
@@ -1,20 +1,11 @@
 { config, pkgs, lib, ... }: {
   imports = [
     ../../modules/partitioning
-    "github:AsahiLinux/nixos-apple-silicon/module"
   ];
 
   # System information
   system = "aarch64-linux";
   networking.hostName = "MBP-M1-VM";
-
-  # Asahi Linux
-  hardware.asahi.audioSupport = true;
-
-  # Enable nonfree packages (for firmware)
-  nixpkgs.config.allowUnfree = [
-    "asahi-fwfetch-*.all"
-  ];
 
   # Partitioning
   partitioning = lib.mkIf (lib.hasSuffix "-PARTITIONED" config.networking.hostName) {
