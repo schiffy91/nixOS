3b476e856a4baf7688502ed89d5c7f2d9b6e8082
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2024-12-18 15:42:25 -0800

    lanzaboote is hell

--- install.sh
+++ install.sh
@@ -5,9 +5,14 @@ if [ ! -d /etc/secureboot ]; then
     mkdir -p /etc/secureboot
     chmod 700 /etc/secureboot
 fi
+
+nix-env -iA niv -f https://github.com/nmattia/niv/tarball/master \
+    --substituters https://niv.cachix.org \
+    --trusted-public-keys niv.cachix.org-1:X32PCg2e/zAm3/uD1ScqW2z/K0LtDyNV7RdaxIuLgQM=
+
 sudo sbctl create-keys
 
 # pull config
 (rm -rf /etc/nixos && cd /etc/ && sudo git clone https://github.com/schiffy91/nixOS.git nixos)
 cd /etc/nixos && sudo niv init && sudo niv add nix-community/lanzaboote -r v0.4.1 -v 0.4.1
-sudo git pull && sudo nixos-rebuild switch --show-trace
+sudo git pull && sudo nixos-rebuild switch -I nixos-config=/etc/nixos/configuration.nix --show-trace
--- modules/boot.nix
+++ modules/boot.nix
@@ -1,28 +1,24 @@
 { config, pkgs, lib, ... }:
 
 let
-    sources = import ./nix/sources.nix;
-    lanzaboote = import sources.lanzaboote;
+  lanzaboote = builtins.getFlake "github:nix-community/lanzaboote";
 in
 {
-  imports = [ lanzaboote.nixosModules.lanzaboote ];
-  environment.systemPackages = [ pkgs.sbctl ];
-  boot = {
-    lanzaboote = {
-      enable = true;
-      pkiBundle = "/etc/secureboot";
-    };
-    loader = {
-      systemd-boot.enable = lib.mkForce false;
-      efi.canTouchEfiVariables = true;
-    };
+  imports = [
+    ./nix/modules/lanzaboote.nix
+    lanzaboote.nixosModules.lanzaboote
+  ];
+
+  # Enable Secure Boot with self-signed keys.
+  boot.lanzaboote = {
+    enable = true;
+    pkiBundle = "/etc/secureboot";
   };
 
-  system.activationScripts.postActivation.text = ''
-    if ! ${pkgs.sbctl}/bin/sbctl status | grep "Setup Mode: false"; then
-      if ! ${pkgs.sbctl}/bin/sbctl verify; then
-        ${pkgs.sbctl}/bin/sbctl enroll-keys --microsoft
-      fi
-    fi
-  '';
+  # This is needed until the `systemd-boot` module is removed.
+  boot.loader.systemd-boot.enable = false;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # For TPM2 support, enable this.
+  virtualisation.tpm.enable = true;
 }
