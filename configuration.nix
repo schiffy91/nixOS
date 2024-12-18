749eec24f84bd014621ade439debf0a0f0690fdd
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2024-12-18 14:04:19 -0800

    fix import

--- configuration.nix
+++ configuration.nix
@@ -3,7 +3,7 @@
 let
   hostConfig = import ./hosts/${"MBP-M1-VM"}.nix { inherit config pkgs lib; };
   partition = true;
-  lanzaboote = import (builtins.fetchTarball { url = "https://github.com/nix-community/lanzaboote/archive/refs/tags/v0.4.1.tar.gz"; }) + "/nixosModules" { inherit pkgs lib; };
+  lanzaboote = import ((builtins.fetchTarball { url = "https://github.com/nix-community/lanzaboote/archive/refs/tags/v0.4.1.tar.gz"; }) + "/nixosModules") { inherit pkgs lib; };
 in
 {
   imports = [
