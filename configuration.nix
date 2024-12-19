17511ecf0f669d94c29e2521e9c81e6547bcecf7
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2024-12-18 18:26:49 -0800

    hopefully oen down

--- configuration.nix
+++ configuration.nix
@@ -5,7 +5,7 @@ let
   disko = import (builtins.fetchTarball {
     url = "https://github.com/nix-community/disko/archive/refs/tags/v1.10.0.tar.gz";
     sha256 = "0jfasvk0hz76cipnv2mfrbgf86gcmnf0niapcm7p37xh7a0w2wxj"; # Replace with the correct hash
-  }) { inherit lib pkgs; };
+  }) { inherit lib; };
 
   # Fetch lanzaboote v0.4.1 from GitHub
   lanzaboote = import (builtins.fetchTarball {
@@ -18,8 +18,8 @@ in
 {
   system.stateVersion = "24.11";
   imports = [
-    disko
-    lanzaboote
+    disko.nixosModules.disko
+    lanzaboote.nixosModules.lanzaboote
     ./modules/drives.nix
     ./modules/boot.nix
     ./modules/desktop.nix
