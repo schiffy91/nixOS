e1b02c795b74e874d682e324f203c3812b9ccec0
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2024-12-18 18:35:48 -0800

    asd

--- configuration.nix
+++ configuration.nix
@@ -1,18 +1,8 @@
 { config, pkgs, lib, ... }:
 
 let
-  # Fetch disko v1.10.0 from GitHub
-  disko = import (builtins.fetchTarball {
-    url = "https://github.com/nix-community/disko/archive/refs/tags/v1.10.0.tar.gz";
-    sha256 = "0jfasvk0hz76cipnv2mfrbgf86gcmnf0niapcm7p37xh7a0w2wxj"; # Replace with the correct hash
-  }) { };
-
-  # Fetch lanzaboote v0.4.1 from GitHub
-  lanzaboote = import (builtins.fetchTarball {
-    url = "https://github.com/nix-community/lanzaboote/archive/refs/tags/v0.4.1.tar.gz";
-    sha256 = "sha256-BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB="; # Replace with the correct hash
-  }) {};
-
+  disko = import ./dependencies/disko/default.nix { inherit lib; };
+  lanzaboote = import ./dependencies/lanzaboote/default.nix { inherit lib pkgs; };
   hostConfig = import ./hosts/${builtins.baseNameOf ./host} { inherit config pkgs lib; };
 in
 {
