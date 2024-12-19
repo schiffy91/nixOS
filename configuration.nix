0fc11d81393c71476e002df0093304bd62149379
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2024-12-18 18:37:09 -0800

    ??

--- configuration.nix
+++ configuration.nix
@@ -1,15 +1,10 @@
 { config, pkgs, lib, ... }:
 
-let
-  disko = import ./dependencies/disko/default.nix { inherit lib; };
-  lanzaboote = import ./dependencies/lanzaboote/default.nix { inherit lib pkgs; };
-  hostConfig = import ./hosts/${builtins.baseNameOf ./host} { inherit config pkgs lib; };
-in
 {
   system.stateVersion = "24.11";
   imports = [
-    disko
-    lanzaboote
+    ./dependencies/disko/default.nix
+    ./dependencies/lanzaboote/default.nix
     ./modules/drives.nix
     ./modules/boot.nix
     ./modules/desktop.nix
@@ -18,6 +13,6 @@ in
     ./modules/networking.nix
     ./modules/sound.nix
     ./modules/users.nix
-    hostConfig
+    ./hosts/${builtins.baseNameOf ./host}
   ];
 }
