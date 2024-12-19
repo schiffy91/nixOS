e55294a2debd247ae7a43c0e76ccbfd28b999b76
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2024-12-18 22:33:19 -0800

    more changes

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ PS3="Select host: "
 select host in MBP-M1-VM FRACTAL-NORTH; do
     [ -z "$host" ] && echo "Invalid choice." && continue
     echo "Building configuration for $host"
-    sudo nixos-rebuild switch --flake ".#$host" --show-trace
+    sudo nixos-rebuild switch --flake ".#$host"
     exit 0
 done
