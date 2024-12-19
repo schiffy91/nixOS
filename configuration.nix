e652014178537fe2a682a40701b387adfbf30f1b
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2024-12-18 18:29:15 -0800

    over this

--- configuration.nix
+++ configuration.nix
@@ -5,13 +5,13 @@ let
   disko = import (builtins.fetchTarball {
     url = "https://github.com/nix-community/disko/archive/refs/tags/v1.10.0.tar.gz";
     sha256 = "0jfasvk0hz76cipnv2mfrbgf86gcmnf0niapcm7p37xh7a0w2wxj"; # Replace with the correct hash
-  }) { inherit lib; };
+  }) { };
 
   # Fetch lanzaboote v0.4.1 from GitHub
   lanzaboote = import (builtins.fetchTarball {
     url = "https://github.com/nix-community/lanzaboote/archive/refs/tags/v0.4.1.tar.gz";
     sha256 = "sha256-BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB="; # Replace with the correct hash
-  }) { inherit lib pkgs; };
+  }) {};
 
   hostConfig = import ./hosts/${builtins.baseNameOf ./host} { inherit config pkgs lib; };
 in
