3bd6882ff49ad7454956cdc38afffb424eb07c2a
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2024-12-18 18:13:48 -0800

    this is why you don't trust robots

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ CONFIG_FILE="$SCRIPT_DIR/configuration.nix"
 PS3="Select host: "
 select hostfile in hosts/*.nix; do
   [ -z "$hostfile" ] && echo "Invalid choice." && continue
-  sudo ln -sf "$SCRIPT_DIR/$hostfile" "$CONFIG_FILE"
+  sudo ln -sf "$SCRIPT_DIR/$hostfile" "host"
   echo "Set config to $hostfile"
   sudo nixos-rebuild switch -I "nixos-config=$CONFIG_FILE" --show-trace
   exit 0
