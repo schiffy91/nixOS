b10e2d876e9a943f008789edf5559cd732960cad
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2024-12-18 17:04:23 -0800

    sudos

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ CONFIG_FILE="$SCRIPT_DIR/configuration.nix"
 PS3="Select host: "
 select hostfile in hosts/*.nix; do
   [ -z "$hostfile" ] && echo "Invalid choice." && continue
-  ln -sf "$SCRIPT_DIR/$hostfile" "$CONFIG_FILE"
+  sudo ln -sf "$SCRIPT_DIR/$hostfile" "$CONFIG_FILE"
   echo "Set config to $hostfile"
-  nixos-rebuild switch -I "nixos-config=$CONFIG_FILE" --show-trace
+  sudo nixos-rebuild switch -I "nixos-config=$CONFIG_FILE" --show-trace
   exit 0
 done
